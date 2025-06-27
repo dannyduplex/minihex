98155bf604d8613d012ba6764e0de993d51bd599
Danny Duplex <dannyduplex@gmail.com>
2025-06-27 09:37:31 -0600

    Update info.js

--- info.js
+++ info.js
@@ -1,18 +1,29 @@
+const popupBackdrop = document.getElementById('popupBackdrop');
 const popup = document.getElementById('popup');
 const infoButton = document.getElementById('infoButton');
 const closeButton = document.getElementById('closePopup');
 
 infoButton.addEventListener('click', (e) => {
 	e.preventDefault();
+	popupBackdrop.classList.add('show');
 	popup.classList.add('show');
 });
 
 closeButton.addEventListener('click', () => {
+	popupBackdrop.classList.remove('show');
 	popup.classList.remove('show');
 });
 
-window.addEventListener('click', (e) => {
-	if (!popup.contains(e.target) && e.target !== infoButton) {
+popupBackdrop.addEventListener('click', (e) => {
+	if (!popup.contains(e.target)) {
+		popupBackdrop.classList.remove('show');
+		popup.classList.remove('show');
+	}
+});
+
+window.addEventListener('keydown', (e) => {
+	if (e.key === 'Escape') {
+		popupBackdrop.classList.remove('show');
 		popup.classList.remove('show');
 	}
 });
